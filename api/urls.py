e080a522190f87b7ed5addbc851a959a617a2205
MacOS <macos@MacBook-Pro-de-MacOS.local>
2023-08-11 19:17:04 +0100

    add update note endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('notes/', views.getNotes, name="notes"),
+    path('notes/<str:id>/update', views.updateNote, name="update-note"),
     path('notes/<str:id>/', views.getSingleNote, name="note")
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Note
 from .serializers import NoteSerializer
 # Create your views here.
@@ -51,4 +52,15 @@ def getNotes(request):
 def getSingleNote(request, id):
     note = Note.objects.get(id=id)
     serializer = NoteSerializer(note, many=False)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["PATCH"])
+def updateNote(request, id):
+    data = request.data
+    note = Note.objects.get(id=id)
+    serializer = NoteSerializer(instance=note, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response("provided data is invalid", status=status.HTTP_400_BAD_REQUEST)
