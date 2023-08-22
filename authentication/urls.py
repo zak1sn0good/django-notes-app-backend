94815aa3f1de3965f27778e0de583d790bac0e49
zak1sn0good <zaki97.usma@gmail.com>
2023-08-22 11:36:17 +0100

    add user-details route

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,8 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import UserDetailsView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user-details/', UserDetailsView.as_view(), name='current-user-details')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+class UserDetailsView(APIView):
+  permission_classes = [IsAuthenticated]
+  def get(self, request, *args, **kwargs):
+    user = User.objects.get(id=request.user.id)
+    serializer = UserSerializer(instance=user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
